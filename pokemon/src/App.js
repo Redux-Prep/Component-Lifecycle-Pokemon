b1a88b5120b0e38c5eef8979c2f70fc7892e8358
Justin Trombley <jtrombley96@gmail.com>
2020-01-14 12:54:47 -0500

    componentDidMount

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -9,11 +9,39 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      pokemon: data
+      pokemon: []
     };
+    console.log('Constructor is running')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount is running')
+    // fetch initial data and set it to state
+    // setup:
+      // event listeners
+      // subscriptions
+      // timers
+    // TASK
+    // update Pokemon state with the data array
+    this.setState({pokemon: data})
+
+  }
+
+  componentWillUnmount() {
+    // To avoid memory leaks
+    // remove/destroy:
+      // event listeners
+      // subscriptions
+      // timers
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate is running')
   }
 
   render() {
+    // Renders things to the DOM
+    console.log('App is rendering')
     return (
       <div className="App">
         <Pokemon pokemon={this.state.pokemon} />
